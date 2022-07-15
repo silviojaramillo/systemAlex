27b995220470d83a0fba102237ff19d78cdec378
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2022-07-14 23:44:59 -0500

    Actualización

--- systemAlex/portafolio/models.py
+++ systemAlex/portafolio/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    id= models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True,verbose_name="Descripción")
+    image = models.ImageField(upload_to='images/', null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
