4b9565c19ab2184a94619a04cd37175bcb5cbabd
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2022-07-16 10:33:20 -0500

    Update

--- systemAlex/portafolio/models.py
+++ systemAlex/portafolio/models.py
@@ -1,10 +1,17 @@
 from distutils.command.upload import upload
+from tabnanny import verbose
 from django.db import models
 
 class Project(models.Model):
-    id= models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
+    id= models.AutoField(primary_key=True, verbose_name="Id")
+    title = models.CharField(max_length=200, verbose_name="Título")
     description = models.TextField(null=True,verbose_name="Descripción")
-    image = models.ImageField(upload_to='images/', null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='projects', null=True, verbose_name="Imagen")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+        ordering = ["-created"]
+    def __str__(self):
+        return self.title
